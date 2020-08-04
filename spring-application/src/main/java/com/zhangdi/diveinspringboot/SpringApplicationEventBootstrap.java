3455d6a3a235e7864c44909d4f3abfc31f30f34c
zhangd <zhangdi013@sina.com>
2020-08-05 00:47:15 +0800

    add listener event

---
+++ spring-application/src/main/java/com/zhangdi/diveinspringboot/SpringApplicationEventBootstrap.java
@@ -0,0 +1,13 @@
+package com.zhangdi.diveinspringboot;
+
+/**
+ * Spring 应用事件引导类
+ *
+ * @author zhangd
+ */
+public class SpringApplicationEventBootstrap {
+
+  public static void main(String[] args) {
+
+  }
+}
